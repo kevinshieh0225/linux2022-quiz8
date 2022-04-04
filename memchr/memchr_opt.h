05170e21c4dbf09d662bc5ab2ea75d2d8a08a498
Demi <minenhsieh@gmail.com>
2022-04-05 01:46:46 +0800

    Try on lfring

--- memchr/memchr_opt.h
+++ memchr/memchr_opt.h
@@ -57,7 +57,7 @@ void *memchr_opt(const void *src_void, int c, size_t length)
             mask = (mask << i) | mask;
 
         while (length >= LBLOCKSIZE) {
-            // if find the char
+            // if find the char, check which char is the target address.
             if (DETECT_CHAR(*asrc, mask) == 0) {
                 src = (unsigned char *) asrc;
                 while (*src != d)
---
+++ shell.sh
@@ -0,0 +1,2 @@
+gcc -g -o ./$1/main.o ./$1/main.c
+./$1/main.o > ./$1/main.txt
